543bd31f1561d30f2eb0bb42b15c25ceec93f2b0
Angela Vu <angeladvu@gmail.com>
2018-03-19 02:21:52 -0700

    Fix queries 4,5,6,7,8 and fix typos, formatting, comments

--- RF/Controllers/HomeController.cs
+++ RF/Controllers/HomeController.cs
@@ -18,40 +18,12 @@ namespace hw7.Controllers
 
             RainerEntities model = new RainerEntities();
             ViewBag.Message = "Query 1: What are the top 10 PC Games by score?";
-            //SELECT TOP 10 IGN_Table.Score, NA_Sales, EU_Sales, Japan_Sales, Other_Sales, Global_Sales FROM IGN_Table JOIN VGSales_Table ON IGN_Table.Title = VGSales_Table.Title WHERE VGSales_Table.Platform = “PC”;
-
-            //var query1 = (from IGN_Table in model.IGN_Table
-            //              join VGSales_Table in model.VGSales_Table
-            //              on IGN_Table.Title equals VGSales_Table.Title
-            //              where IGN_Table.Platform == "PC"
-            //              orderby IGN_Table.Score descending
-            //              select IGN_Table).Take(10);
 
             var query1 = (from IGN_Table in model.IGN_Table
                           where IGN_Table.Platform == "PC"
                           orderby IGN_Table.Score descending
                           select IGN_Table
                           ).Take(10);
-
-            //var query2 = (from IGN_Table in model.IGN_Table
-            //              orderby IGN_Table.Score descending
-            //              group IGN_Table by IGN_Table.Score into IGN_Scores
-            //              select IGN_Table
-            //              );
-
-            //var query2 = (from IGN_Table in model.IGN_Table
-            //             group IGN_Table by IGN_Table.Score into IGN_Scores
-            //             orderby IGN_Scores.Key
-            //             select IGN_Scores);
-
-            //WITH Top10Games_CTE(Title, Platform, Score)
-            //    AS(
-            //    SELECT TOP 10 Title, Platform, Score
-            //    FROM IGN_Table)
-            //    Select COUNT(Title) AS 'Number of Top 10 PC Games'
-            //    FROM Top10Games_CTE
-            //    WHERE Platform = 'PC'
-
 
             List<IGN_Table> IGNList = query1.ToList();
 
@@ -63,7 +35,7 @@ namespace hw7.Controllers
         {
 
             RainerEntities model = new RainerEntities();
-            ViewBag.Message = "Query 2: What are the platform(s) for top 5 selling games Globally?";
+            ViewBag.Message = "Query 2: What are the platform(s) for top 5 selling games globally?";
 
             var query2 = (from VGSales_Table in model.VGSales_Table
                           orderby VGSales_Table.Global_Sales descending
@@ -96,27 +68,14 @@ namespace hw7.Controllers
 
             RainerEntities model = new RainerEntities();
             ViewBag.Message = "Query 4: Of Top 10 PC games in NA what were the different ratings?";
-            //SELECT TOP 10 VGSales_Table.Title, NA_Sales, SteamSpy_Table.Score, IGN_Table.Score from VGSales_Table JOIN SteamSpy_Table ON VGSales_Table.Title = SteamSpy_Table.Title JOIN IGN_Table ON SteamSpy_Table.Title = IGN_Table.Title;
 
-            //SELECT TOP 5
-            //VGSales_Table.Title, NA_Sales, IGN_Table.Score AS[IGN Score],
-            // Steamspy_Table.[Metacritic Score],
-            // SteamSpy_Table.[User Score] AS[Steam User Score],
-            // Steamspy_Table.[Average Score Rank]
-            //        AS[Steam Average Score]
-            //                  FROM VGSales_Table 
-            //                  JOIN SteamSpy_Table ON VGSales_Table.GameID = SteamSpy_Table.GameID
-            //                  JOIN IGN_Table ON VGSales_Table.GameID = IGN_Table.GameID
-            //ORDER BY NA_Sales DESC;
-
-            //TODO: this sorta works, can't get IGN SCORE though
             var query4 = (from Steamspy_Table in model.Steamspy_Table
                           join VGSales_Table in model.VGSales_Table on Steamspy_Table.GameID equals VGSales_Table.GameID
                           join IGN_Table in model.IGN_Table on Steamspy_Table.GameID equals IGN_Table.GameID
                           orderby VGSales_Table.NA_Sales descending, IGN_Table.Score descending
                           select Steamspy_Table
                           ).Take(5);
-      
+
             List<Steamspy_Table> SteamList = query4.ToList();
 
             return View(SteamList);
@@ -127,7 +86,6 @@ namespace hw7.Controllers
 
             RainerEntities model = new RainerEntities();
             ViewBag.Message = "Query 5: How many titles are Ubisoft games?";
-            //SELECT TOP 10 Title, NA_Sales, EU_Sales, Japan_Sales, Other_Sales, Global_Sales  FROM VGSales_Table JOIN SteamSpy_Table ON VGSales_Table.Title = SteamSpy_Table.Title WHERE Publisher LIKE “Valve”;
 
             var query5 = (from VGSales_Table in model.VGSales_Table
                           where VGSales_Table.Publisher.Contains("Ubisoft")
@@ -143,17 +101,12 @@ namespace hw7.Controllers
         {
 
             RainerEntities model = new RainerEntities();
-            ViewBag.Message = "Query 6:  What genre has sold the most copies?";
-            //SELECT VGSales_Table.Genre, COUNT (VGSales_Table.Genre) AS Genre_Popularity, NA_Sales, EU_Sales, Japan_Sales, Other_Sales, Global_Sales  FROM VGSales_Table GROUP BY VGSales_Table.Genre ORDER BY Genre_Popularity DESC LIMIT 1;
-
+            ViewBag.Message = "Query 6: What game has sold the most copies?";
 
             var query6 = (from VGSales_Table in model.VGSales_Table
-                          //group vg.Genre by vg.Genre into Genre_Popularity
-                          //orderby VGSales_Table.Genre.Count() descending
+                          orderby VGSales_Table.Global_Sales descending
                           select VGSales_Table).Take(1);
-
-                          //select VGSales_Table).GroupBy(VGSales_Table => VGSales_Table.Genre).Select(grp => new { Genre = grp.Key, Count = grp.Count() }).OrderBy(x => x.Genre);
-
+           
             List<VGSales_Table> VGList = query6.ToList();
 
             return View(VGList);
@@ -163,8 +116,7 @@ namespace hw7.Controllers
         {
 
             RainerEntities model = new RainerEntities();
-            ViewBag.Message = "Query 7:  What PC games were released in March across all tables?";
-            //SELECT IGN_Table.Title, Month From IGN_Table JOIN SteamSpy_Table ON IGN_Table.Title = SteamSpy_Table.Title JOIN VGsales_Table ON SteamSpy_Table.Title = VGSales_Table.Title WHERE Month = “5” AND IGN.Title = SteamSpy_Table.Title AND SteamSpy_Table.Title = VGSales_Table.Title;
+            ViewBag.Message = "Query 7: What games were released in March (all tables)?";
 
             var query7 = (from IGN_Table in model.IGN_Table
                           join VGSales_Table in model.VGSales_Table on IGN_Table.GameID equals VGSales_Table.GameID
@@ -174,17 +126,6 @@ namespace hw7.Controllers
 
             List<IGN_Table> IGNList = query7.ToList();
 
-            //var query7 = (from VGSales_Table in model.VGSales_Table
-            //              join IGN_Table in model.IGN_Table on VGSales_Table.GameID equals IGN_Table.GameID
-            //              join Steamspy_Table in model.Steamspy_Table on IGN_Table.GameID equals Steamspy_Table.GameID // on IGN_Table.GameID equals Steamspy_Table.GameID
-            //              where Steamspy_Table.Month == 3
-            //              //group new { VGSales_Table, IGN_Table, Steamspy_Table } by new { Steamspy_Table.GameID, Steamspy_Table.Title, Steamspy_Table.Platform, Steamspy_Table.Month, Steamspy_Table.Year } into Games_Group
-            //              //select new { Games_Group.Key.GameID, Games_Group.Key.Title, Games_Group.Key.Platform, Games_Group.Key.Month, Games_Group.Key.Year }).Take(10);
-            //              select VGSales_Table).Take(10);
-
-            //List<VGSales_Table> VGList = query7.ToList();
-
-
             return View(IGNList);
         }
 
@@ -192,8 +133,7 @@ namespace hw7.Controllers
         {
 
             RainerEntities model = new RainerEntities();
-            ViewBag.Message = "Query 8: What is the Top 5  rated IGN  games sold ?";
-            //SELECT * FROM IGN_Table ORDER BY Score LIMIT 5;
+            ViewBag.Message = "Query 8: What are the Top 5 rated IGN games?";
 
             var query8 = (from IGN_Table in model.IGN_Table
                           orderby IGN_Table.Score descending
@@ -203,7 +143,7 @@ namespace hw7.Controllers
 
             return View(IGNList);
         }
-        
+
 
     }
 }
